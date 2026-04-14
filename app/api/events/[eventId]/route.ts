d33f246a1479199774e9dc504d438df22e13809e
Raghvendra9402 <singhraghvendra405@gmail.com>
2026-04-14 20:47:23 +0530

    chore: update type definition for params in DELETE function to use Promise

--- app/api/events/[eventId]/route.ts
+++ app/api/events/[eventId]/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 
 export async function DELETE(
   req: Request,
-  { params }: { params: { eventId: string } }
+  { params }: { params: Promise<{ eventId: string }> },
 ) {
   try {
     const { userId } = await auth();
